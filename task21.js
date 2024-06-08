f178cb4ee91d09385fd4d90915a5379e0047fb1a
Mac <mac@Uzair-Ahmed-2.local>
2024-06-08 12:41:37 +0500

    Task23

--- task21.js
+++ task21.js
@@ -1,2 +1,7 @@
-export {};
 // 21. They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+var person = {
+    name: "Uzair",
+    fName: "Syed Willayat Ali",
+    gender: "male"
+};
+console.log(person);
--- task21.ts
+++ task21.ts
@@ -1,2 +1,13 @@
-
 // 21. They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+let person: {
+  name: string;
+  fName: string;
+  gender: string;
+} = {
+  name: "Uzair",
+  fName: "Syed Willayat Ali",
+  gender: "male",
+};
+
+console.log(person);
